35289e46262c80ca0a1ffa9c98addd83fc7ad32a
henapartae123 <liyulabs@gmail.com>
2025-10-20 17:01:49 +0300

    test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -106,6 +106,22 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         """Stop patching."""
         cls.get_patcher.stop()
 
+    def test_public_repos(self):
+        """Integration test for public_repos returns expected repo names."""
+        client = GithubOrgClient("test")
+        self.assertEqual(
+            sorted(client.public_repos()),
+            sorted(self.expected_repos)
+        )
+
+    def test_public_repos_with_license(self):
+        """Integration test for filtering by license."""
+        client = GithubOrgClient("test")
+        self.assertEqual(
+            sorted(client.public_repos("apache-2.0")),
+            sorted(self.apache2_repos)
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
