8f6f4212273d02755e608dec2d1af9309592dc23
henapartae123 <liyulabs@gmail.com>
2025-10-21 09:52:52 +0300

    models

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,30 @@
+# messaging/models.py
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User, related_name='sent_messages', on_delete=models.CASCADE
+    )
+    receiver = models.ForeignKey(
+        User, related_name='received_messages', on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Message from {self.sender} to {self.receiver}"
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, related_name='notifications', on_delete=models.CASCADE, null=True, blank=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username}"
+
+
